0f38530bba633561f27232353512cf4ee1cbd37e
Ankita Roy <38494404+ankitaroy21@users.noreply.github.com>
2019-07-12 14:43:25 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,26 @@
 import React from 'react';
-
 class Footer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {addClass: false};
     }
-    toggle() {
-        this.setState({addClass: !this.state.addClass});
+    toggle(event) {
+        var elems = document.querySelectorAll(".selected");
+        [].forEach.call(elems, function(el) {
+            el.classList.remove(".selected");
+        });
+        var elem=event.target;
+        elem.classList.add(".selected")
     }
     render() {
+        let showAll=<button id="all"className="selected"onClick={this.toggle.bind(this)}>ALL</button>;
+        let showAct=<button id="active"onClick={this.toggle.bind(this)}>ACTIVE</button>;
+        let showComp=<button id="completed"onClick={this.toggle.bind(this)}>COMPLETED</button>
         return (
             <footer>
-                <p id="noOfItems"></p>
-                <button id="all"className="selected"onclick={this.toggle()}>ALL</button>
-                <button id="active"onclick={this.toggle()}>ACTIVE</button>
-                <button id="completed"onclick={this.toggle()}>COMPLETED</button>
+                {showAll}
+                {showAct}
+                {showComp}
             </footer>
         );
     }
